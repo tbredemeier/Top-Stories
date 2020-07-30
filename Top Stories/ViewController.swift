72df29368a0e57e0e7f2e893e93ccb398c6af2fd
Tom Bredemeier <tbredemeier@barrington220.org>
2020-07-30 10:58:20 -0500

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by tbredemeier on 7/30/20.
+//  Copyright © 2020 tbredemeier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
